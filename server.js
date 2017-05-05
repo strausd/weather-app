bf52e50ba8825e70903bd99ac3ffa967a929d7d5
Daniel Straus <strausd@me.com>
2017-05-04 21:00:28 -0500

    Move options obj inside development if block for https

--- server.js
+++ server.js
@@ -7,10 +7,6 @@ app.use(express.static('public'));
 
 process.env.NODE_ENV = process.env.NODE_ENV || 'development';
 
-var options = {
-   key  : fs.readFileSync('key.pem'),
-   cert : fs.readFileSync('cert.pem')
-};
 
 var port = process.env.PORT || 3000;
 
@@ -19,6 +15,10 @@ app.get('/', (req, res) => {
 });
 
 if(process.env.NODE_ENV === 'development') {
+    var options = {
+        key  : fs.readFileSync('key.pem'),
+        cert : fs.readFileSync('cert.pem')
+    };
     https.createServer(options, app).listen(port, () => {
         console.log(`Server started on port ${port}`);
     });
